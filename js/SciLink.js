db1913a8cc02c9e3c8b3a7b5cb11dffa6d7392b3
lilidream <ubuntullmx@hotmail.com>
2022-05-12 17:55:34 +0800

    add obejct and process nodes

--- js/SciLink.js
+++ js/SciLink.js
@@ -18,20 +18,28 @@ class SciLink{
         </rect>
         <text style={{fill: '#000',  textAlign:'center', fontSize:12, marginLeft:${(cfg.height * cfg.ratio + cfg.padding*2) / 2}}} name="title">${cfg.label}</text>
       </group>
-    `)
+    `);
+    
+    G6.registerNode('process', (cfg) => `
+      <group>
+        <rect style={{width:${cfg.width + cfg.padding*2}, height:${cfg.height + cfg.padding*2}, stroke: '${cfg.color}',fill: '${cfg.color}', textAlign:'center', radius:[6,6,6,6]}}  name="equation" keyShape='true'>
+          <text style={{fill: '#fff',  textAlign:'left', fontSize:12, marginLeft:${(cfg.padding*2)}, marginTop:${(cfg.height/2+cfg.padding*2)}}} name="title">${cfg.content}</text>
+        </rect>
+      </group>
+    `);
     // 默认为力布局
     option.layout = option.layout || {
         type: 'gForce',
         prevenOverlap: true,
         linkDistance: 200,
-        minMovement: 0.1,
-        maxIteration: 1000,
+        minMovement: 0.001,
+        maxIteration: 10000,
       };
     option.modes = option.modes || {
       default: ['drag-canvas', 'zoom-canvas', 'drag-node']
     }
     option.defaultNode = option.defaultNode || {
-          type: 'dom-node',
+          type: 'circle ',
           size: [120, 40],
     }
   
@@ -58,6 +66,19 @@ class SciLink{
     }
   }
 
+  insertNewLine(text, n){
+    var i;
+    var l = text.length;
+    var lineNumber = 0;
+    for(i=0; i<l; i++){
+      if((i+1)%n == 0){
+        text = text.slice(0, i) + "\n" + text.slice(i);
+        lineNumber += 1
+      }
+    }
+    return {text: text, lineNumber: lineNumber};
+  }
+
 
   // 转换数据为G6格式
   transformData(data){
@@ -66,7 +87,8 @@ class SciLink{
     var i, d, node;
     var colors = {
       element: "#C4DCEB",
-      equation: "#46B692"
+      equation: "#46B692",
+      process: "#ba7ece"
     }
 
     // nodes
@@ -82,11 +104,24 @@ class SciLink{
         padding: d.padding || 5,
         fontSize: d.fontSize || 12
       }
-      if (d.type == 'element'||'equation'){
+      if (d.type == 'element'|| d.type == 'equation'){
         var tex = d.latex || d.id;
         var svg = this.latex2Svg(tex);
         node.base64 = svg.base64;
         node.ratio = svg.ratio;
+      }else if (d.type == 'object'){
+        node.type = 'circle',
+        node.size = d.size || 20+d.name.length*11
+        node.style = {
+          fill: '#d6ad85',
+          stroke: '#835f50',
+        }
+      }else if (d.type == 'process'){
+        let textNumber = d.wrap || 12
+        let t = this.insertNewLine(d.content, textNumber);
+        node.content = t.text;
+        node.height = t.lineNumber * 18;
+        node.width = textNumber*12
       }
       g6Data.nodes.push(node);
     }
